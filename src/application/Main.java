4ffc1b1933fbd4d44b92c3b99e2b181d63175802
Divay Prakash <divayprakash3@gmail.com>
2016-04-02 19:18:42 +0530

    Updated Main.java to load FrontEnd

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,33 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
+	private Stage primaryStage;
+	private AnchorPane pane;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			this.primaryStage = primaryStage;
+			this.primaryStage.setTitle("Art Gallery DBMS");
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("./FrontEnd.fxml"));
+			pane = (AnchorPane)loader.load();
+			Scene scene = new Scene(pane);
 			primaryStage.setScene(scene);
+			primaryStage.setWidth(600);
+			primaryStage.setHeight(600);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 	public static void main(String[] args) {
 		launch(args);
 	}
