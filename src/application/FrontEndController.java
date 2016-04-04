a1a3c1488265eb000b6f7afd2a15eb28942aa007
Divay Prakash <divayprakash3@gmail.com>
2016-04-04 21:55:29 +0530

    Artist tab done, updated fxml

--- src/application/FrontEndController.java
+++ src/application/FrontEndController.java
@@ -1,5 +1,47 @@
 package application;
 
-public class FrontEndController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FrontEndController implements Initializable {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		//initialize function
+	}
+	//boolean variable to enable switching tabs
+	private boolean tabSelection = false;
+	/*
+	 * Function to check tab and set bool variable
+	 */
+	private boolean checkTab() {
+	    tabSelection = !tabSelection;
+	    return tabSelection;
+	}
+	/*
+	 * Artist Tab
+	 */
+	@FXML private TextField artistFirstName;
+	@FXML private TextField artistLastName;
+	@FXML private TextField artistCity;
+	@FXML private TextField artistCountry;
+	@FXML private DatePicker artistDOB;
+	@FXML private TextArea artistStyles;
+	@FXML private Button artistData;
+	@FXML void handleArtistTab(Event event) {
+		if (checkTab()) {
+			//handle initialization for artist tab
+		}
+	}
+	@FXML void handleUpdateButtonAction(ActionEvent event) {
+		//handle artist data submit
+	}
 }
